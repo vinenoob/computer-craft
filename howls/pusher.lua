7b6ab39bd23525c6cb9b861b56b0ecf8b00ae09a
Jonathan Nielsen <vinenoob@gmail.com>
2020-11-30 00:41:01 -0700

    fixed not adding stuff

--- howls/pusher.lua
+++ howls/pusher.lua
@@ -1,5 +1,26 @@
 rednet.open("left")
 
+function split (inputstr, sep)
+    if sep == nil then
+            sep = "%s"
+    end
+    local t={}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+            table.insert(t, str)
+    end
+    return t
+end
+
+function parseArgs(args)
+    info = {}
+
+    splitArgs = split(args)
+    info["who"] = splitArgs[0]
+    info["what"] = splitArgs[1]
+
+    return (info)
+end
+
 function push ()
     redstone.setOutput("bottom", false)
 end
