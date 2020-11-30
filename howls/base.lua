3027ab122a5b1dce54de1e55f91f08cc5e90f0af
Jonathan Nielsen <vinenoob@gmail.com>
2020-11-30 02:23:30 -0700

    renamed

---
+++ howls/base.lua
@@ -0,0 +1,44 @@
+rednet.open("left")
+
+function split (inputstr, sep)
+    if sep == nil then
+            sep = "%s"
+    end
+    local t={}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+            table.insert(t, str)
+    end
+    return t
+end
+
+function parseArgs(args)
+    info = {}
+
+    splitArgs = split(args, ",")
+    info["who"] = splitArgs[1]
+    info["what"] = splitArgs[2]
+
+    return (info)
+end
+
+function push ()
+    redstone.setOutput("back", true)
+end
+
+function pull ()
+    redstone.setOutput("back", false)
+end
+
+local myName = "puller"
+
+while true do
+    senderId, message, distance = rednet.receive()
+    local info = parseArgs(message)
+    if info["who"] == myName then
+        if info["what"] == "push" then
+            push()
+        elseif info["what"] == "pull" then
+            pull()
+        end
+    end
+end
