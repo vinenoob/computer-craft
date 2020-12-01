6f9d9103b574471a0c0a5a77644923a90669c8b8
Jonathan Nielsen <vinenoob@gmail.com>
2020-11-30 22:05:40 -0700

    added distance

--- howls/controller.lua
+++ howls/controller.lua
@@ -15,17 +15,24 @@ function sendCommand (str1, str2)
     os.sleep(2.5)
 end
 
+turtle.equipLeft()
+shell.execute("base")
+shell.execute("motor")
 
 while true do
-    print("what direction")
+    print("what direction?")
     local direction = read()
     local dMotor = direction.." "..motor
     local dBase = direction.." "..base
-    sendCommand(dMotor, breakBlock)
-    sendCommand(dMotor, extend)
-    sendCommand(dMotor, retract)
-    sendCommand(dBase, extend)
-    sendCommand(dBase, retract)
-    sendCommand(dMotor, placeBlock)
+    print("how far?")
+    local distance = tonumber(read())
+    for i=distance, 1, -1 do
+        sendCommand(dMotor, breakBlock)
+        sendCommand(dMotor, extend)
+        sendCommand(dMotor, retract)
+        sendCommand(dBase, extend)
+        sendCommand(dBase, retract)
+        sendCommand(dMotor, placeBlock)
+    end
 end
 
