71a0f7897851ba28e5a2d72503ce5fe8ea9e4228
Jonathan Nielsen <vinenoob@gmail.com>
2021-08-01 22:45:37 -0600

    different mining

--- mining.lua
+++ mining.lua
@@ -3,6 +3,25 @@ for i = 100, 1, -1 do
     turtle.forward()
 end
 
+turtle.turnRight()
+turtle.dig()
+turtle.forward()
+turtle.dig()
+turtle.forward()
+turtle.dig()
+turtle.forward()
+turtle.turnRight()
+
 for i = 100, 1, -1 do
-    turtle.back()
+    turtle.dig()
+    turtle.forward()
 end
+
+turtle.turnLeft()
+turtle.dig()
+turtle.forward()
+turtle.dig()
+turtle.forward()
+turtle.dig()
+turtle.forward()
+turtle.turnLeft()
